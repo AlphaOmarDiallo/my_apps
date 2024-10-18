b04992652c47e8175054f0c3b1cb5a5ee4ff1d34
Alpha <adiallo@exomind.fr>
2024-10-18 17:50:13 +0200

    return list project

---
+++ App.py
@@ -0,0 +1,6 @@
+class App:
+    def __init__(self, img_resource: str, description: str, name: str, playstore_link: str):
+        self.img_resource = img_resource
+        self.description = description
+        self.name = name
+        self.playstore_link = playstore_link
